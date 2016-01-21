976c7c8bb793c0dca5c0432d05f5e271ad36b8a0
kandros <kandros5591@gmail.com>
2016-01-21 17:22:47 +0100

    handle double click

--- background.js
+++ background.js
@@ -1,11 +1,30 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var alreadyClicked = false;
+var timer;
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var sound = chrome.extension.getURL("/bravoh.wav");
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (alreadyClicked) {
+        clearTimeout(timer);
+        console.log("Double click");
+        var sound = chrome.extension.getURL("/pureio.wav");
+        playSound(sound);
+        alreadyClicked = false;
+        return;
+    }
+
+    alreadyClicked = true;
+
+    timer = setTimeout(function () {
+        console.log("Single click");
+        var sound = chrome.extension.getURL("/bravoh.wav");
+        playSound(sound);
+        clearTimeout(timer);
+        alreadyClicked = false;
+    }, 250);
+});
+
+
+function playSound(sound) {
   var audio = new Audio(sound);
   audio.currentTime = 0;
   audio.play();
-});
+}
