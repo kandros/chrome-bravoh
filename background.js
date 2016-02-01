4f973716a896a7019b5991974be948beb0b9fc6a
agifuni <antonio.gifuni@accenture.com>
2016-02-01 16:13:15 +0100

    soralella

--- background.js
+++ background.js
@@ -2,26 +2,33 @@ var alreadyClicked = false;
 var timer;
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    if (alreadyClicked) {
-        clearTimeout(timer);
-        console.log("Double click");
-        var sound = chrome.extension.getURL("/pureio.wav");
-        playSound(sound);
-        alreadyClicked = false;
-        return;
-    }
+  if (alreadyClicked) {
+    clearTimeout(timer);
+    console.log("Double click");
+    var sound = chrome.extension.getURL("/pureio.wav");
+    playSound(sound);
+    alreadyClicked = false;
+    return;
+  }
 
-    alreadyClicked = true;
+  alreadyClicked = true;
 
-    timer = setTimeout(function () {
-        console.log("Single click");
-        var sound = chrome.extension.getURL("/bravoh.wav");
-        playSound(sound);
-        clearTimeout(timer);
-        alreadyClicked = false;
-    }, 250);
+  timer = setTimeout(function () {
+    console.log("Single click");
+    var sound = chrome.extension.getURL("/bravoh.wav");
+    playSound(sound);
+    clearTimeout(timer);
+    alreadyClicked = false;
+  }, 250);
+
 });
-
+//soraLella
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === "soraLella") {
+    var sound = chrome.extension.getURL("/soralella.wav");
+    playSound(sound);
+  }
+});
 
 function playSound(sound) {
   var audio = new Audio(sound);
